bc7afbdf03a07e165c2aad485c7e9311caae4976
Goutham R <gouthamr@hcltech.com>
2025-05-24 22:03:53 +0530

    updated messages

--- modules/lambda/lambda_function/handler.py
+++ modules/lambda/lambda_function/handler.py
@@ -1,13 +1,35 @@
-import json
 import boto3
+import json
+import os
 import urllib.parse
 import requests
 
-def main(event, context):
-    s3 = boto3.client('s3')
+s3 = boto3.client('s3')
+FASTAPI_URL = os.environ.get("http://13.203.102.146:8000/ci-logs")
+
+def lambda_handler(event, context):
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = urllib.parse.unquote_plus(record['s3']['object']['key'])
-        response = s3.get_object(Bucket=bucket, Key=key)
-        logs = response['Body'].read().decode('utf-8')
-        requests.post("https://your-api-endpoint.com/ci-logs", json={"key": key, "logs": logs})
+
+        try:
+            response = s3.get_object(Bucket=bucket, Key=key)
+            logs = response['Body'].read().decode('utf-8')
+
+            print(f"Log file content from S3 ({key}):")
+            print(logs)
+
+            # Send to FastAPI endpoint
+            if FASTAPI_URL:
+                api_response = requests.post(
+                    FASTAPI_URL,
+                    headers={"Content-Type": "application/json"},
+                    data=json.dumps({"key": key, "logs": logs})
+                )
+                print("API response:", api_response.text)
+            else:
+                print("FASTAPI_URL is not set")
+
+        except Exception as e:
+            print(f"Error processing file {key} from bucket {bucket}. Error: {e}")
+            raise e
